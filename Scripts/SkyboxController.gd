769e09e87edcd2b0c2b7bc8165496f45cbca49ed
Radagasd <42610517+Radagasd@users.noreply.github.com>
2024-11-17 14:34:58 +0000

    Chapter 4.3

--- Scripts/SkyboxController.gd
+++ Scripts/SkyboxController.gd
@@ -3,7 +3,10 @@ extends WorldEnvironment # We're modifying the sky material that is on a WorldEn
 
 func _process(delta: float) -> void:
 	var sun_dir = $"../Sun".get_global_transform().basis.z; # This is our forward direction pointing towards the sun
-	var moon_dir = $"../Moon".get_global_transform().basis.z; # This is our forward direction pointing towards the moon
+	var moonBasis = $"../Moon".get_global_transform().basis;
+	var moon_dir = moonBasis.z; # This is our forward direction pointing towards the moon
 	environment.sky.sky_material.set_shader_parameter('_SunDir', sun_dir); # Update sky material with sun direction
 	environment.sky.sky_material.set_shader_parameter('_MoonDir', moon_dir); # Update sky material with moon direction
+	moonBasis.x = -moonBasis.x;
+	environment.sky.sky_material.set_shader_parameter('_MoonSpaceMatrix', moonBasis);
 	pass
