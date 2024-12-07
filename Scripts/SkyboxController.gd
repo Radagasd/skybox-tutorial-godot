2e0150ce75588fc5d326c41cfb2e34739632e46a
Radagasd <42610517+Radagasd@users.noreply.github.com>
2024-12-07 14:42:26 +0000

    snake_case all uniforms

--- Scripts/SkyboxController.gd
+++ Scripts/SkyboxController.gd
@@ -3,9 +3,9 @@ extends WorldEnvironment # We're modifying the sky material that is on a WorldEn
 
 func _process(delta: float) -> void:
 	var sun_dir = $"../Sun".get_global_transform().basis.z; # This is our forward direction pointing towards the sun
-	var moonBasis = $"../Moon".get_global_transform().basis;
-	var moon_dir = moonBasis.z; # This is our forward direction pointing towards the moon
-	environment.sky.sky_material.set_shader_parameter('_SunDir', sun_dir); # Update sky material with sun direction
-	environment.sky.sky_material.set_shader_parameter('_MoonDir', moon_dir); # Update sky material with moon direction
-	environment.sky.sky_material.set_shader_parameter('_MoonWorldToObject', moonBasis.inverse()); # The world to object matrix is the inverse of the basis (which is object to world)
+	var moon_basis = $"../Moon".get_global_transform().basis;
+	var moon_dir = moon_basis.z; # This is our forward direction pointing towards the moon
+	environment.sky.sky_material.set_shader_parameter('sun_dir', sun_dir); # Update sky material with sun direction
+	environment.sky.sky_material.set_shader_parameter('moon_dir', moon_dir); # Update sky material with moon direction
+	environment.sky.sky_material.set_shader_parameter('moon_world_to_object', moon_basis.inverse()); # The world to object matrix is the inverse of the basis (which is object to world)
 	pass
